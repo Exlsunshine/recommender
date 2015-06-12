3f6b3d48d047a1f46edc45f62d74a139d52fdda0
sunshine <sunshine46582747@sina.com>
2015-06-12 17:07:04 +0800

    Add new function: calc_user_similarity_from_gene(normalized_user_gene_file_path).

--- Python codes/Phase2/twoLayerRecoomendOriginal.py
+++ Python codes/Phase2/twoLayerRecoomendOriginal.py
@@ -5,12 +5,59 @@ import numpy
 import math
 
 
-def calc_item_similarity_from_gene(item_gene_file_path):
+def calc_user_similarity_from_gene(normalized_user_gene_file_path):
+    # Load raw user gene data.
+    user_number = 0
+    gene_feature_number = 0
+    raw_data = []
+    with open(normalized_user_gene_file_path) as f:
+        for line in f:
+            line = line.replace('\n', '')
+            raw_data.append(line)
+            user_number += 1
+            gene_feature_number = len(line.split('\t'))
+    f.close()
+    print 'Success [Load user gene data complete]'
+
+    # Format row user gene data into n by m numpy matrix.
+    user_gene = numpy.zeros((user_number, gene_feature_number), dtype=float)
+    for i in xrange(0, user_number):
+        values = raw_data[i].split('\t')
+        for j in xrange(0, gene_feature_number):
+            user_gene[i, j] = values[j]
+    print 'Success [Format item gene data complete]'
+
+    # Drop the user_id feature data.
+    user_gene = numpy.delete(user_gene, 0, 1)
+
+    # Calculate user similarity by user' gene data.
+    user_similarity = numpy.zeros((user_number, user_number), dtype=float)
+    for i in xrange(0, user_number):
+        print 'Similarity computing:\t' + str(round(1.0 * i / user_number * 100, 2))
+        for j in xrange(0, user_number):
+            if i == j:
+                user_similarity[i, j] = 0
+            elif i < j:
+                user_similarity[i, j] = round(cosine_similarity(user_gene[i], user_gene[j]), 2)
+                user_similarity[j, i] = user_similarity[i, j]
+    print 'Success [Similarity computation task complete]'
+
+    # Save similarity data to file.
+    with open('../dataset/output/user_gene_similarity_data.txt', 'w+') as f:
+        for i in xrange(0, user_number):
+            line = ""
+            for j in xrange(0, user_number):
+                line += user_similarity[i][j].__str__() + '\t'
+            f.write(line[:-1] + '\n')
+    print 'Success [Saving data to file complete]'
+
+
+def calc_item_similarity_from_gene(normalized_item_gene_file_path):
     # Load raw item gene data.
     item_number = 0
     gene_feature_number = 0
     row_data = []
-    with open(item_gene_file_path) as f:
+    with open(normalized_item_gene_file_path) as f:
         for line in f:
             line = line.replace('\n', '')
             row_data.append(line)
@@ -58,5 +105,8 @@ def cosine_similarity(v1, v2):
 
 
 if __name__ == '__main__':
-    item_gene_data_path = 'D:/Data/Graduate_1_Spring/Recommender System/TestCase/Python codes/dataset/output/normalized_item_genes_data.txt'
-    calc_item_similarity_from_gene(item_gene_data_path)
+    # normalized_item_gene_data_path = '../dataset/output/normalized_item_genes_data.txt'
+    # calc_item_similarity_from_gene(normalized_item_gene_data_path)
+
+    normalized_user_gene_data_path = '../dataset/output/normalized_user_genes_data.txt'
+    calc_user_similarity_from_gene(normalized_user_gene_data_path)
